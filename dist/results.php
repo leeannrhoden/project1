a8ac2ccad508adbf56467f2fa1b076134027bd40
leeannrhoden <31945397+leeannrhoden@users.noreply.github.com>
2017-10-17 00:48:40 -0700

    night 10-16

--- dist/results.php
+++ dist/results.php
@@ -6,7 +6,7 @@
 <div class="container mx-auto">
 	<!-- Start of Card -->
 	<div class="card border-0">
-		<img src="images/<?php  echo  $icon_current ?>.svg" alt="" width="200px" class="mx-auto">
+		<img src="images/<?php  echo  $icon_current ?>.svg" alt="" width="300px" class="mx-auto">
 		
 		<h2 class="mx-auto"> 
 			<?php echo $place; ?>
@@ -23,16 +23,32 @@
 	<!-- End of Card -->
 
 	<!-- Start of Experiment -->
-		<div class="bg-danger rounded my-5 py-3">
-		 <!--  <p> this is the expirement section</p> -->
-		  	<div class="title bg-white my-5 mx-5 rounded">
+		<div class=" bg-danger rounded my-4 py-3">
+
+		 	<!--  experiment title-->
+		  	<div class="title bg-white my-3 mx-5 py-4 px-2 rounded">
 		  		Suck an Egg into a bottle
+		  	</div>
+
+		  	<!-- MATERIALS-->
+		  	<div class="materials bg-white my-4 mx-5 py-4 px-2 rounded" width="200px" >
 		  		
+		  	</div>
 
-
+		  	<!-- DRAWING -->
+		  	<div class=" drawing bg-white my-4 mx-5 py-4 px-2 rounded" >
+		  		
 		  	</div>
 			
+			<!-- STEPS -->
+			<div class="steps bg-white my-4 mx-5 py-4 px-2 rounded">
+				
+			</div>
 
+			<!-- EXPLANATION -->
+			<div class="explanation bg-white my-4 mx-5 py-4 px-2 rounded" width="200px">
+				
+			</div>
 
 
 
